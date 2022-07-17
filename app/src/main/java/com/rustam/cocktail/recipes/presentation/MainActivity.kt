dda3c7d350a8d8d7a468bbc12f63d9d05998b122
arhangel <rustambakytov7@gmail.com>
2022-07-18 01:00:14 +0900

    Added App , SplashActivity classes and moved to separate packages

--- app/src/main/java/com/rustam/cocktail/recipes/presentation/MainActivity.kt
+++ app/src/main/java/com/rustam/cocktail/recipes/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.rustam.cocktail.recipes
+package com.rustam.cocktail.recipes.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rustam.cocktail.recipes.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
